fc362034f0714937d23bb0f002ab323740588601
G.Mustafa Shaikh <34856975+TheMustafaShaikh@users.noreply.github.com>
2020-07-09 00:46:14 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Number from "./number";
 import './App.css';
 
 function App() {
-    return ( <
-        div className = "App" >
-        <
-        /div>
+
+  const [number,setNumber] = useState(0);
+  const [color,setColor] = useState(false);
+
+  const changeColor = () =>{
+      if(color){
+        document.getElementById("main").style.backgroundColor= 'blue';
+        setColor(!color);
+      }else{
+        document.getElementById("main").style.backgroundColor= 'gray';
+        setColor(!color);
+      }
+  }
+  
+    return ( 
+      <div className = "App" id="back">
+        <div id="main">
+          <h1>Click the number to show numbers:</h1>
+          <Number numbers={number}/>
+          <button onClick={()=> setNumber(number+1)}>Click this</button>
+          <br />
+          <br />
+          <button onClick={changeColor}> Change color</button>
+        </div>
+      </div>
     );
 }
 
---
+++ src/number.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function number(props) {
+    return (
+        <div>
+            <h1>{props.numbers}</h1>
+        </div>
+    )
+}
